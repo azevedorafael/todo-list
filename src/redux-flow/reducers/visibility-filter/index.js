b29cdd82351c7ea79df594c8eda028831e0a56e4
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2018-06-20 00:01:30 -0300

    Connect filter to state

--- src/redux-flow/reducers/visibility-filter/index.js
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -1,7 +1,7 @@
 import { SET_VISIBILITY_FILTER, SHOW_ALL } from './actions'
 
 
-export const initialState = 'SHOW_ALL'
+export const initialState = SHOW_ALL
 
 const visibilityFilter = (state = initialState, action) => {
     switch (action.type) {
